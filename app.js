49bf8271213ba6cd060cab5f672155beb9aaf8fd
Kulkarni Code <87711046+kulkarniCode@users.noreply.github.com>
2021-10-21 13:21:50 +0530

    added modal

---
+++ app.js
@@ -0,0 +1,8 @@
+const dateOfBirth = document.querySelector("#txtDob");
+const luckyNumber = document.querySelector("#txtLuckyNo");
+
+const modal = document.querySelector("#modal");
+const modalBody = document.querySelector("#modalBody");
+const close = document.querySelector(".close");
+
+const checkBtn = document.querySelector("#checkBtn");
